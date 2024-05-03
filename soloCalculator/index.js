e885414a8ffdaf4f27afdd8bb9d555ec7227f6f6
Steven Klimek <steven.klimek7@yahoo.com>
2024-05-03 12:24:42 -0500

    finally

--- soloCalculator/index.js
+++ soloCalculator/index.js
@@ -14,4 +14,24 @@ const darkModeButton = document.querySelector('.dark');
         document.documentElement.classList.add('light')
     })
 
-    // want to change background image on click of light/dark mode
+
+// Define the openUrl function
+function openUrl(url) {
+    // Open the URL in a new tab
+    window.open(url, "_blank");
+}
+
+// Get the element with the class "falsezero"
+const falseZeroButton = document.querySelector('.falsezero');
+
+// Get the result box element
+const resultBox = document.querySelector('.resultbox');
+
+// Add a click event listener to the "00" button
+falseZeroButton.addEventListener('click', function() {
+    // Call the openUrl function with the YouTube URL
+    openUrl('https://www.youtube.com/watch?v=2qBlE2-WL60');
+    
+    // Display the message in the result box
+    resultBox.children[1].value = "RICK ROLLED!";
+});
